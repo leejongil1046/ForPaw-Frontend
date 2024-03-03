ad63c190df2fc8e07900b65f977f22b478869e99
mayuuna <jjyjjh33@gmail.com>
2024-03-03 12:31:55 +0900

    loading & login page

---
+++ pages/loading/index.js
@@ -0,0 +1,9 @@
+import LoadingUI from "../../src/components/units/loading/Loading.presenter";
+
+export default function Loadinng(){
+  return(
+    <>
+      <LoadingUI />
+    </>
+  )
+}
---
+++ pages/login/index.js
@@ -0,0 +1,9 @@
+import LogInUI from "../../src/components/units/login/login.presenter";
+
+export default function LogIn() {
+  return (
+    <>
+      <LogInUI />
+    </>
+  )
+}
