c311a2ba936d3b78c065b1aa4d1208f30ffa306e
leejongil1046@gmail.com <leejongil1046@gmail.com>
2024-03-09 02:01:54 +0900

    feat: 봉사활동 페이지의 지역별 모임 select 메뉴 추가

--- src/components/units/volunteer/region/VolunteerRegion.presenter.js
+++ src/components/units/volunteer/region/VolunteerRegion.presenter.js
@@ -1,9 +1,39 @@
 import * as S from "./VolunteerRegion.styles";
+import Image from "next/image";
 
 export default function VolunteerRegionUI() {
   return (
     <>
-      <S.WrapperContents></S.WrapperContents>
+      <S.WrapperContents>
+        <S.VolunteerRegionMenuBlock>
+          <S.VolunteerRegionMenu>
+            <option value="" disabled selected>
+              지역명
+            </option>
+          </S.VolunteerRegionMenu>
+          <S.MenuArrowBlock>
+            <Image
+              src="/images/volunteer/volunteer_arrow_down_icon.svg"
+              alt="volunteer_arrow_down_icon"
+              width={18}
+              height={20}
+            />
+          </S.MenuArrowBlock>
+          <S.VolunteerRegionMenuSub>
+            <option value="" disabled selected>
+              지역명
+            </option>
+          </S.VolunteerRegionMenuSub>
+          <S.MenuSubArrowBlock>
+            <Image
+              src="/images/volunteer/volunteer_arrow_down_icon.svg"
+              alt="volunteer_arrow_down_icon"
+              width={18}
+              height={20}
+            />
+          </S.MenuSubArrowBlock>
+        </S.VolunteerRegionMenuBlock>
+      </S.WrapperContents>
     </>
   );
 }
--- src/components/units/volunteer/region/VolunteerRegion.styles.js
+++ src/components/units/volunteer/region/VolunteerRegion.styles.js
@@ -19,3 +19,72 @@ export const WrapperContents = styled.div`
   }
   scrollbar-width: none; /* Firefox */
 `;
+
+export const VolunteerRegionMenuBlock = styled.div`
+  width: 240px;
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  margin-top: 15px;
+  margin-right: 100px;
+`;
+
+export const VolunteerRegionMenu = styled.select`
+  width: 115px;
+  height: 44px;
+  border-radius: 22px;
+  background-color: white;
+  border: 1px solid #f5f5f5;
+  box-shadow: 1px 0 2px 1px rgba(0, 0, 0, 0.05),
+    0 2px 2px 1px rgba(0, 0, 0, 0.15);
+  font-size: 20px;
+  padding-left: 15px;
+  position: relative;
+
+  -webkit-appearance: none; /* 크롬, 사파리 */
+  -moz-appearance: none; /* 파이어폭스 */
+  appearance: none; /* 표준 */
+`;
+
+export const VolunteerRegionMenuSub = styled.select`
+  width: 115px;
+  height: 44px;
+  border-radius: 22px;
+  background-color: white;
+  border: 1px solid #f5f5f5;
+  box-shadow: 1px 0 2px 1px rgba(0, 0, 0, 0.05),
+    0 2px 2px 1px rgba(0, 0, 0, 0.15);
+  font-size: 20px;
+  padding-left: 15px;
+
+  -webkit-appearance: none; /* 크롬, 사파리 */
+  -moz-appearance: none; /* 파이어폭스 */
+  appearance: none; /* 표준 */
+`;
+
+export const MenuArrowBlock = styled.div`
+  width: 30px;
+  height: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 13px;
+  left: 70px;
+`;
+
+export const MenuSubArrowBlock = styled.div`
+  width: 30px;
+  height: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 13px;
+  left: 195px;
+`;
