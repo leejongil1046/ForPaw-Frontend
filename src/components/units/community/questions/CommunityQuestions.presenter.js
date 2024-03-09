a9004800f804ee727df5c1aae960a9c50a31cdee
leejongil1046@gmail.com <leejongil1046@gmail.com>
2024-03-10 00:21:37 +0900

    feat: 커뮤니티 페이지의 궁금해요 페이지 추가

--- src/components/units/community/questions/CommunityQuestions.presenter.js
+++ src/components/units/community/questions/CommunityQuestions.presenter.js
@@ -5,6 +5,126 @@ export default function CommunityQuestionsUI() {
   return (
     <>
       <S.WrapperContents>
+        <S.CommunityQuestionBlock style={{ backgroundColor: "#fff0eb" }}>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
+        <S.CommunityQuestionBlock>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
+        <S.CommunityQuestionBlock>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
+        <S.CommunityQuestionBlock>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
+        <S.CommunityQuestionBlock>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
+        <S.CommunityQuestionBlock>
+          <S.QuestionInfoBlock>
+            <S.QuestionProfile>
+              <Image
+                src="/images/community/community_questions_profile.svg"
+                alt="community_questions_profile"
+                width={40}
+                height={40}
+              />
+              닉네임
+            </S.QuestionProfile>
+            <S.QuestionDate>24.02.16</S.QuestionDate>
+          </S.QuestionInfoBlock>
+          <S.QuestionTtile>갑자기 궁금한 게 생겨서 그런데요</S.QuestionTtile>
+          <S.QuestionText>
+            질문 내용을 상세하게 적어주세요. 동해물과 백두산이 마르고 닳도록
+            하느님이 보우하사 우리 나라 만세
+          </S.QuestionText>
+          <S.NumberOfAnswers>답변 2</S.NumberOfAnswers>
+        </S.CommunityQuestionBlock>
         <S.CommunityAddIcon>
           <Image
             src="/images/community/community_add_icon.svg"
--- src/components/units/community/questions/CommunityQuestions.styles.js
+++ src/components/units/community/questions/CommunityQuestions.styles.js
@@ -8,6 +8,8 @@ export const WrapperContents = styled.div`
   justify-content: flex-start;
   align-items: center;
   margin: 0 auto;
+  padding-top: 15px;
+  padding-bottom: 10px;
   overflow-y: auto;
   background-color: white;
   border-left: 1px solid black;
@@ -18,6 +20,67 @@ export const WrapperContents = styled.div`
     display: none;
   }
   scrollbar-width: none; /* Firefox */
+`;
+
+export const CommunityQuestionBlock = styled.div`
+  width: 344px;
+  height: 219px;
+  background-color: #f6f6f6;
+  border-radius: 20px;
+  margin-top: 10px;
+  flex-shrink: 0;
+`;
+
+export const QuestionInfoBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  padding: 0 12px;
+`;
+
+export const QuestionProfile = styled.div`
+  height: 40px;
+  line-height: 40px;
+  font-size: 16px;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const QuestionDate = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: #807876;
+`;
+
+export const QuestionTtile = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+  margin-left: 12px;
+`;
+
+export const QuestionText = styled.div`
+  width: 286px;
+  font-size: 16px;
+  font-weight: 300;
+  margin-top: 12px;
+  margin-left: 12px;
+`;
+
+export const NumberOfAnswers = styled.div`
+  width: 100%;
+  font-size: 16px;
+  font-weight: 400;
+  color: #807876;
+  text-align: end;
+  padding-right: 12px;
 `;
 
 export const CommunityAddIcon = styled.div`
