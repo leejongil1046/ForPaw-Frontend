44621e157a75c768b10b573a7f22d7d0dbf9c70a
leejongil1046@gmail.com <leejongil1046@gmail.com>
2024-03-12 03:24:06 +0900

    feat: 입양 문의하기 페이지 추가

--- src/components/units/adopt/detail/inquire/Inquire.styles.js
+++ src/components/units/adopt/detail/inquire/Inquire.styles.js
@@ -45,13 +45,154 @@ export const Title = styled.h1`
   margin-left: 10px;
 `;
 
-export const WrapperAlarm = styled.div`
+export const WrapperInquire = styled.div`
   width: 390px;
   height: calc(100vh - 95px);
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   margin: 0 auto;
   background-color: white;
+  overflow-y: auto;
+  padding-bottom: 20px;
+
+  /* 스크롤 바 숨기기 */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  scrollbar-width: none; /* Firefox */
+`;
+
+export const InfoContainer = styled.div`
+  width: 342px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex-shrink: 0;
+`;
+
+export const InfoTitle = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+  margin-top: 30px;
+`;
+
+export const InfoLabel = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+  margin-bottom: 15px;
+`;
+
+export const InfoLargeBlock = styled.div`
+  width: 342px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const InfoLargeInput = styled.input`
+  width: 342px;
+  height: 44px;
+  background-color: #fef8f2;
+  border: 2px solid transparent;
+  outline: none;
+  border-radius: 10px;
+  font-size: 16px;
+  padding-left: 12px;
+
+  ::placeholder {
+    color: #bea597;
+  }
+
+  :focus {
+    border: 2px solid #bea597;
+    color: #bea597;
+  }
+`;
+
+export const GenderAgeBlock = styled.div`
+  width: 342px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InfoSmallBlock = styled.div`
+  width: 163px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const InfoSmallInput = styled.input`
+  width: 163px;
+  height: 44px;
+  background-color: #fef8f2;
+  border: 2px solid transparent;
+  outline: none;
+  border-radius: 10px;
+  font-size: 16px;
+  padding-left: 12px;
+  margin-top: 15px;
+
+  ::placeholder {
+    color: #bea597;
+  }
+
+  :focus {
+    border: 2px solid #bea597;
+    color: #bea597;
+  }
+`;
+
+export const InfoAddressSearchBlock = styled.div`
+  width: 342px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InfoMiddleInput = styled.input`
+  width: 253px;
+  height: 44px;
+  background-color: #fef8f2;
+  border: 2px solid transparent;
+  outline: none;
+  border-radius: 10px;
+  font-size: 16px;
+  padding-left: 12px;
+  margin-bottom: 10px;
+
+  ::placeholder {
+    color: #bea597;
+  }
+
+  :focus {
+    border: 2px solid #bea597;
+    color: #bea597;
+  }
+`;
+
+export const InfoSearchButton = styled.button`
+  width: 73px;
+  height: 44px;
+  background-color: #240d05;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const InquireButton = styled.button`
+  background-color: #ff6636;
+  color: #ffffff;
+  margin-top: 50px;
 `;
