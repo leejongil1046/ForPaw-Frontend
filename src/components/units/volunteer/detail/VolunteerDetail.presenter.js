73d8b018c06d115918cc615183988c96f7ad39e5
mayuuna <jjyjjh33@gmail.com>
2024-03-14 02:32:27 +0900

    feat: 봉사모임 상세 페이지 정기모임 전체적인 레이아웃 완성

--- src/components/units/volunteer/detail/VolunteerDetail.presenter.js
+++ src/components/units/volunteer/detail/VolunteerDetail.presenter.js
@@ -30,27 +30,83 @@ export default function VolunteerDetailUI() {
             </S.RightArrowImgBlock>
           </S.AnnouncementTitleBlock>
           <S.AnnouncementDetailContainer>
-            <S.AnnouncementDetailBlock style={{backgroundColor: "#FEF8F2"}}>
-              <S.CheckBox>
-                <S.CheckImg>
-                  <Image
-                    src="/images/volunteer/volunteerDetail/check_icon.svg"
-                    alt="check_icon"
-                    width={22}
-                    height={15}
-                  />
-                </S.CheckImg>
-              </S.CheckBox>
-              <S.AnnouncementDetail>
-                이번주 봉사활동은 업체의 개인사정으로 인해 한 주<br />
-                쉬어가니 착오 없으시길 바랍니다.
-              </S.AnnouncementDetail>
+            <S.AnnouncementDetailBlock style={{ backgroundColor: "#FEF8F2" }}>
+              <S.AnnouncementItems>
+                <S.CheckBox>
+                  <S.CheckImg>
+                    <Image
+                      src="/images/volunteer/volunteerDetail/check_icon.svg"
+                      alt="check_icon"
+                      width={22}
+                      height={15}
+                    />
+                  </S.CheckImg>
+                </S.CheckBox>
+                <S.AnnouncementDetail>
+                  이번주 봉사활동은 업체의 개인사정으로 인해 한 주<br />
+                  쉬어가니 착오 없으시길 바랍니다.<br />
+                </S.AnnouncementDetail>
+              </S.AnnouncementItems>
+            </S.AnnouncementDetailBlock>
+            <S.AnnouncementDetailBlock>
+              <S.AnnouncementItems>
+                <S.CheckBox>
+                  <S.CheckImg>
+                    <Image
+                      src="/images/volunteer/volunteerDetail/check_icon.svg"
+                      alt="check_icon"
+                      width={22}
+                      height={15}
+                    />
+                  </S.CheckImg>
+                </S.CheckBox>
+                <S.AnnouncementDetail>
+                  이번주 봉사활동은 업체의 개인사정으로 인해 한 주<br />
+                  쉬어가니 착오 없으시길 바랍니다.<br />
+                </S.AnnouncementDetail>
+              </S.AnnouncementItems>
+            </S.AnnouncementDetailBlock>
+            <S.AnnouncementDetailBlock>
+              <S.AnnouncementItems>
+                <S.CheckBox>
+                  <S.CheckImg>
+                    <Image
+                      src="/images/volunteer/volunteerDetail/check_icon.svg"
+                      alt="check_icon"
+                      width={22}
+                      height={15}
+                    />
+                  </S.CheckImg>
+                </S.CheckBox>
+                <S.AnnouncementDetail>
+                  이번주 봉사활동은 업체의 개인사정으로 인해 한 주<br />
+                  쉬어가니 착오 없으시길 바랍니다.<br />
+                </S.AnnouncementDetail>
+              </S.AnnouncementItems>
             </S.AnnouncementDetailBlock>
           </S.AnnouncementDetailContainer>
         </S.AnnouncementContainer>
-        <S.NextButtonBlock>
-          <S.NextButtonItem>가입하기</S.NextButtonItem>
-        </S.NextButtonBlock>
+        <S.MeetingContainer>
+          <S.MeetingTitleBlock>
+            <S.MeetingTitle>정기모임</S.MeetingTitle>
+            <S.RightArrowImgBlock>
+              <Image
+                src="/images/volunteer/volunteerDetail/right_arrow.svg"
+                alt="right_arrow"
+                width={44}
+                height={44}
+              />
+            </S.RightArrowImgBlock>
+          </S.MeetingTitleBlock>
+          <S.WrapperMeetingDetail>
+            <S.MeetingDetailContainer>
+              <S.MeetingDetailContentsBox>
+
+              </S.MeetingDetailContentsBox>
+            </S.MeetingDetailContainer>
+          </S.WrapperMeetingDetail>
+        </S.MeetingContainer>
+        
       </S.WrapperContents>
     </>
   )
--- src/components/units/volunteer/detail/VolunteerDetail.styles.js
+++ src/components/units/volunteer/detail/VolunteerDetail.styles.js
@@ -74,14 +74,29 @@ export const RightArrowImgBlock = styled.div`
 
 export const AnnouncementDetailContainer = styled.div`
   width: 100%;
-  height: 187px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
   margin-top: 6px;
+  gap: 8px;
 `;
 
 export const AnnouncementDetailBlock = styled.div`
-  width: 344px;
+  width: 343px;
   height: 57px;
+  background-color: #F6F6F6;
+  border: 1px solid #F6F2EE;
   border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const AnnouncementItems = styled.div`
+  width: 306px;
+  height: 38px;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -93,8 +108,7 @@ export const CheckBox = styled.div`
   height: 38px;
   border-radius: 50%;
   background-color: #FF6636;
-  margin-right: 5px;
-
+  
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -107,27 +121,68 @@ export const CheckImg = styled.div`
 `;
 
 export const AnnouncementDetail = styled.p`
-  width: 276px;
+  width: 267px;
   height: 38px;
   font-size: 14px;
-  letter-spacing: -0.2px;
-  padding-left: 5px;
+  letter-spacing: -0.7px;
+  margin-left: 10px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
- export const ContentsContainer = styled.div`
+export const MeetingContainer = styled.div`
   width: 344px;
-  height: 244px;
-  background-color: #fef8f2;
-  border: 1px solid #f6f2ee;
-  border-radius: 20px;
+  margin-top: 40px;
+  cursor: pointer;
+`;
+
+export const MeetingTitleBlock = styled.div`
+  width: 100%;
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const MeetingTitle = styled.p`
+  width: 300px;
+  height: 44px;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 0px;
+`;  
+
+export const WrapperMeetingDetail = styled.div`
+  width:100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  align-items: flex-start;
+  margin-top: 6px;
+  gap: 8px;
+`;
+
+export const MeetingDetailContainer = styled.div`
+  width: 343px;
+  height: 233px;
+  background-color: #FEF8F2;
+  border: 1px solid #F6F2EE;
+  border-radius: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  margin-top: 12px;
-  flex-shrink: 0;
-  cursor: pointer;
- `;
+`;
+
+export const MeetingDetailContentsBox = styled.div`
+  width: 312px;
+  height: 193px;
+
+  display: flex;
+  flex-direction: column;
+
+`;
 
 /*버튼 css*/
 export const NextButtonBlock = styled.div`
