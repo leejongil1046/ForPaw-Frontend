072674df711459ef78ab933a712bbdef893968cf
mayuuna <jjyjjh33@gmail.com>
2024-03-21 22:15:46 +0900

    feat: announcement의 좋아요 토글 기능 추가

--- src/components/units/volunteer/announcements/announcement/Announcement.container.js
+++ src/components/units/volunteer/announcements/announcement/Announcement.container.js
@@ -1,11 +1,41 @@
 import AnnouncementUI from "./Announcement.presenter";
 import VolunteerDetailHeader from "../../detail/volunteerDetailHeader/VolunteerDetailHeader.container";
+import { useState } from "react";
+import * as S from "./Announcement.styles";
+import Image from "next/image";
 
 export default function Announcement() {
+
+  const LikeImage = ({ initialSrc, alt }) => {
+    const [imgSrc, setImgSrc] = useState(initialSrc);
+    const [isSecondClick, setIsSecondClick] = useState(false);
+
+    const handleImgClick = () => {
+      const newSrc = isSecondClick
+        ? "/images/volunteer/announcement/comment_like_icon.svg"
+        : "/images/volunteer/announcement/comment_active_like_icon.svg";
+      setImgSrc(newSrc);
+      setIsSecondClick(!isSecondClick);
+    };
+
+    return (
+      <S.LikeImg onClick={handleImgClick}>
+        <Image
+          src={imgSrc}
+          alt={alt}
+          width={30}
+          height={30}
+        />
+      </S.LikeImg>
+    );
+  };
+
   return (
     <>
       <VolunteerDetailHeader />
-      <AnnouncementUI />
+      <AnnouncementUI 
+        LikeImage={LikeImage}
+      />
     </>
   )
 }
