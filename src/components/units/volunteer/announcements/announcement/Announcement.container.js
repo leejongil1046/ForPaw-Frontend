5ced7a3fe1678b2ed662d7cd6a1c72422f60e898
mayuuna <jjyjjh33@gmail.com>
2024-03-22 22:38:25 +0900

    pref: 공지사항 상세 페이지 댓글 기능 업그레이드

--- src/components/units/volunteer/announcements/announcement/Announcement.container.js
+++ src/components/units/volunteer/announcements/announcement/Announcement.container.js
@@ -1,41 +1,61 @@
-import AnnouncementUI from "./Announcement.presenter";
-import VolunteerDetailHeader from "../../detail/volunteerDetailHeader/VolunteerDetailHeader.container";
-import { useState } from "react";
-import * as S from "./Announcement.styles";
-import Image from "next/image";
+import LikeImage from './component/LikeImage'; // LikeImage 컴포넌트의 파일 경로에 따라 수정해야 할 수도 있습니다.
+import AnnouncementUI from './Announcement.presenter';
+import VolunteerDetailHeader from '../../detail/volunteerDetailHeader/VolunteerDetailHeader.container';
+import { useState } from 'react';
+import { Reply } from './Announcement.styles';
 
 export default function Announcement() {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-  const LikeImage = ({ initialSrc, alt }) => {
-    const [imgSrc, setImgSrc] = useState(initialSrc);
-    const [isSecondClick, setIsSecondClick] = useState(false);
-
-    const handleImgClick = () => {
-      const newSrc = isSecondClick
-        ? "/images/volunteer/announcement/comment_like_icon.svg"
-        : "/images/volunteer/announcement/comment_active_like_icon.svg";
-      setImgSrc(newSrc);
-      setIsSecondClick(!isSecondClick);
-    };
-
-    return (
-      <S.LikeImg onClick={handleImgClick}>
-        <Image
-          src={imgSrc}
-          alt={alt}
-          width={30}
-          height={30}
-        />
-      </S.LikeImg>
-    );
+  const handleMenuClick = () => {
+    setIsMenuClicked(true);
   };
 
+  const handleOutsideMenuClick = () => {
+    setIsMenuClicked(false);
+  };
+  
+  const initialComments = [
+    {
+      id: 1,
+      name: "닉네임1",
+      region: "부산",
+      hours: "4시간전",
+      text: "댓글 내용을 입력해주세요",
+      replys: [
+        { id: 1, name: "닉네임1", region: "서울", hours: "3시간전", text: "댓글 내용을 입력해주세요" }
+      ]
+    },
+    {
+      id: 2,
+      name: "닉네임2",
+      region: "부산",
+      hours: "5시간전",
+      text: "댓글 내용을 입력해주세요",
+      replys: [
+        { id: 1, name: "닉네임2", region: "경기", hours: "2시간전", text: "댓글 내용을 입력해주세요" },
+        { id: 2, name: "닉네임2", region: "대구", hours: "1시간전", text: "댓글 내용을 입력해주세요" }
+      ]
+    }
+  ];
+
+  const replys = initialComments.map(comment => comment.replys);
+
+  console.log(replys);
+  
   return (
     <>
-      <VolunteerDetailHeader />
-      <AnnouncementUI 
+      <VolunteerDetailHeader
+        isMenuClicked={isMenuClicked}
+        handleMenuClick={handleMenuClick}
+        handleOutsideMenuClick={handleOutsideMenuClick}
+      />
+      <AnnouncementUI
         LikeImage={LikeImage}
+        handleOutsideMenuClick={handleOutsideMenuClick}
+        initialComments={initialComments}
+        replys={replys}
       />
     </>
-  )
+  );
 }
