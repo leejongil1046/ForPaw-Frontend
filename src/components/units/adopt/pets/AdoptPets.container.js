0b6d7fecb19ca417c8780b1f1afd76157163872a
leejongil1046@gmail.com <leejongil1046@gmail.com>
2024-03-24 20:35:47 +0900

    feat: 입양/임시보호 페이지 GET 통신 추가

--- src/components/units/adopt/pets/AdoptPets.container.js
+++ src/components/units/adopt/pets/AdoptPets.container.js
@@ -3,16 +3,86 @@ import Headers from "../../../commons/headers/Headers.container";
 import Navigation from "../../../commons/navigation/Navigation.container";
 import AdoptHandler from "../AdoptHandler.container";
 import AdpotPetsUI from "./AdoptPets.presenter";
+import { useEffect, useState } from "react";
+import { fetchPets } from "./AdoptPets.queries";
+
+const petsData = [
+  {
+    id: 1,
+    name: "인절미",
+    age: "5살",
+    location: "부산 금정구",
+    type: "dog",
+    genderIcon: "/images/pets/male_icon_big.svg",
+    likeCount: 12,
+    commentCount: 25,
+    imageSrc: "/images/pets/dog_1.svg",
+  },
+  {
+    id: 2,
+    name: "콩이",
+    age: "3살",
+    location: "서울 강남구",
+    type: "cat",
+    genderIcon: "/images/pets/male_icon_big.svg",
+    likeCount: 18,
+    commentCount: 30,
+    imageSrc: "/images/pets/dog_1.svg",
+  },
+  {
+    id: 3,
+    name: "두부",
+    age: "2살",
+    location: "인천 연수구",
+    type: "dog",
+    genderIcon: "/images/pets/male_icon_big.svg",
+    likeCount: 8,
+    commentCount: 14,
+    imageSrc: "/images/pets/dog_1.svg",
+  },
+  {
+    id: 4,
+    name: "단비",
+    age: "4살",
+    location: "대구 중구",
+    type: "dog",
+    genderIcon: "/images/pets/male_icon_big.svg",
+    likeCount: 20,
+    commentCount: 35,
+    imageSrc: "/images/pets/dog_1.svg",
+  },
+  {
+    id: 5,
+    name: "모찌",
+    age: "6살",
+    location: "경기 수원시",
+    type: "dog",
+    genderIcon: "/images/pets/male_icon_big.svg",
+    likeCount: 15,
+    commentCount: 22,
+    imageSrc: "/images/pets/dog_1.svg",
+  },
+];
 
 export default function AdpotPets() {
   const router = useRouter();
+  const [pets, setPets] = useState(petsData);
+
+  useEffect(() => {
+    async function fetchData() {
+      // const petsData = await fetchPets();
+      // setPets(petsData);
+    }
+
+    fetchData();
+  }, []);
 
   const navigateTo = (path) => () => router.push(path);
   return (
     <>
       <Headers />
       <AdoptHandler />
-      <AdpotPetsUI navigateTo={navigateTo} />
+      <AdpotPetsUI navigateTo={navigateTo} pets={pets} />
       <Navigation />
     </>
   );
--- src/components/units/adopt/pets/AdoptPets.presenter.js
+++ src/components/units/adopt/pets/AdoptPets.presenter.js
@@ -2,226 +2,63 @@ import * as S from "./AdoptPets.styles";
 import Image from "next/image";
 
 export default function AdpotPetsUI(props) {
+  // `props.pets` 내의 각 요소를 순회하여 UI 구성
   return (
     <>
       <S.WrapperContents>
-        <S.AdoptPetMenuBlock>
-          <S.AdoptPetMenuDate>최근 날짜</S.AdoptPetMenuDate>
-          <S.AdoptPetMenuDogs>개</S.AdoptPetMenuDogs>
-          <S.AdoptPetMenuCats>고양이</S.AdoptPetMenuCats>
-        </S.AdoptPetMenuBlock>
-        <S.AdoptPet onClick={props.navigateTo("/adopt/detail")}>
-          <Image
-            src="/images/pets/dog_1.svg"
-            alt="dog_1"
-            width={344}
-            height={344}
-          />
-          <S.AdoptLikeToggle>
-            <Image
-              src="/images/pets/like_icon_big.svg"
-              alt="like_icon_big"
-              width={32}
-              height={32}
-            />
-          </S.AdoptLikeToggle>
-          <S.AdoptInfoBlock>
-            <S.AdoptNameGender>
-              유기동물 이름
+        {props.pets.map((pet) => (
+          <S.AdoptPet
+            key={pet.id}
+            onClick={props.navigateTo(`/adopt/pets/detail`)}
+          >
+            <Image src={pet.imageSrc} alt={pet.name} width={344} height={344} />
+            <S.AdoptLikeToggle>
               <Image
-                src="/images/pets/male_icon_big.svg"
-                alt="male_icon_big"
-                width={24}
-                height={24}
+                src="/images/pets/like_icon_big.svg"
+                alt="like_icon_big"
+                width={32}
+                height={32}
               />
-            </S.AdoptNameGender>
-            <S.AdoptText>
-              사람을 좋아하는 사교성, 친화력 최고 인절미
-            </S.AdoptText>
-            <S.AdoptBirthAddress>5살 부산 금정구</S.AdoptBirthAddress>
-            <S.AdoptLikeBlock>
-              <S.AdoptLike>
+            </S.AdoptLikeToggle>
+            <S.AdoptInfoBlock>
+              <S.AdoptNameGender>
+                {pet.name}
                 <Image
-                  src="/images/pets/like_icon.svg"
-                  alt="like_icon"
-                  width={20}
-                  height={20}
+                  src={pet.genderIcon}
+                  alt="gender_icon"
+                  width={24}
+                  height={24}
                 />
-              </S.AdoptLike>
-              12
-            </S.AdoptLikeBlock>
-            <S.AdoptViewBlock>
-              <S.AdoptView>
-                <Image
-                  src="/images/pets/comment_icon.svg"
-                  alt="comment_icon"
-                  width={16}
-                  height={16}
-                />
-              </S.AdoptView>
-              25
-            </S.AdoptViewBlock>
-          </S.AdoptInfoBlock>
-        </S.AdoptPet>
-        <S.AdoptPet onClick={props.navigateTo("/adopt/pets/detail")}>
-          <Image
-            src="/images/pets/dog_1.svg"
-            alt="dog_1"
-            width={344}
-            height={344}
-          />
-          <S.AdoptLikeToggle>
-            <Image
-              src="/images/pets/like_icon_big.svg"
-              alt="like_icon_big"
-              width={32}
-              height={32}
-            />
-          </S.AdoptLikeToggle>
-          <S.AdoptInfoBlock>
-            <S.AdoptNameGender>
-              유기동물 이름
-              <Image
-                src="/images/pets/male_icon_big.svg"
-                alt="male_icon_big"
-                width={24}
-                height={24}
-              />
-            </S.AdoptNameGender>
-            <S.AdoptText>
-              사람을 좋아하는 사교성, 친화력 최고 인절미
-            </S.AdoptText>
-            <S.AdoptBirthAddress>5살 부산 금정구</S.AdoptBirthAddress>
-            <S.AdoptLikeBlock>
-              <S.AdoptLike>
-                <Image
-                  src="/images/pets/like_icon.svg"
-                  alt="like_icon"
-                  width={20}
-                  height={20}
-                />
-              </S.AdoptLike>
-              12
-            </S.AdoptLikeBlock>
-            <S.AdoptViewBlock>
-              <S.AdoptView>
-                <Image
-                  src="/images/pets/comment_icon.svg"
-                  alt="comment_icon"
-                  width={16}
-                  height={16}
-                />
-              </S.AdoptView>
-              25
-            </S.AdoptViewBlock>
-          </S.AdoptInfoBlock>
-        </S.AdoptPet>
-        <S.AdoptPet onClick={props.navigateTo("/adopt/pets/detail")}>
-          <Image
-            src="/images/pets/dog_1.svg"
-            alt="dog_1"
-            width={344}
-            height={344}
-          />
-          <S.AdoptLikeToggle>
-            <Image
-              src="/images/pets/like_icon_big.svg"
-              alt="like_icon_big"
-              width={32}
-              height={32}
-            />
-          </S.AdoptLikeToggle>
-          <S.AdoptInfoBlock>
-            <S.AdoptNameGender>
-              유기동물 이름
-              <Image
-                src="/images/pets/male_icon_big.svg"
-                alt="male_icon_big"
-                width={24}
-                height={24}
-              />
-            </S.AdoptNameGender>
-            <S.AdoptText>
-              사람을 좋아하는 사교성, 친화력 최고 인절미
-            </S.AdoptText>
-            <S.AdoptBirthAddress>5살 부산 금정구</S.AdoptBirthAddress>
-            <S.AdoptLikeBlock>
-              <S.AdoptLike>
-                <Image
-                  src="/images/pets/like_icon.svg"
-                  alt="like_icon"
-                  width={20}
-                  height={20}
-                />
-              </S.AdoptLike>
-              12
-            </S.AdoptLikeBlock>
-            <S.AdoptViewBlock>
-              <S.AdoptView>
-                <Image
-                  src="/images/pets/comment_icon.svg"
-                  alt="comment_icon"
-                  width={16}
-                  height={16}
-                />
-              </S.AdoptView>
-              25
-            </S.AdoptViewBlock>
-          </S.AdoptInfoBlock>
-        </S.AdoptPet>
-        <S.AdoptPet onClick={props.navigateTo("/adopt/pets/detail")}>
-          <Image
-            src="/images/pets/dog_1.svg"
-            alt="dog_1"
-            width={344}
-            height={344}
-          />
-          <S.AdoptLikeToggle>
-            <Image
-              src="/images/pets/like_icon_big.svg"
-              alt="like_icon_big"
-              width={32}
-              height={32}
-            />
-          </S.AdoptLikeToggle>
-          <S.AdoptInfoBlock>
-            <S.AdoptNameGender>
-              유기동물 이름
-              <Image
-                src="/images/pets/male_icon_big.svg"
-                alt="male_icon_big"
-                width={24}
-                height={24}
-              />
-            </S.AdoptNameGender>
-            <S.AdoptText>
-              사람을 좋아하는 사교성, 친화력 최고 인절미
-            </S.AdoptText>
-            <S.AdoptBirthAddress>5살 부산 금정구</S.AdoptBirthAddress>
-            <S.AdoptLikeBlock>
-              <S.AdoptLike>
-                <Image
-                  src="/images/pets/like_icon.svg"
-                  alt="like_icon"
-                  width={20}
-                  height={20}
-                />
-              </S.AdoptLike>
-              12
-            </S.AdoptLikeBlock>
-            <S.AdoptViewBlock>
-              <S.AdoptView>
-                <Image
-                  src="/images/pets/comment_icon.svg"
-                  alt="comment_icon"
-                  width={16}
-                  height={16}
-                />
-              </S.AdoptView>
-              25
-            </S.AdoptViewBlock>
-          </S.AdoptInfoBlock>
-        </S.AdoptPet>
+              </S.AdoptNameGender>
+              <S.AdoptText>
+                {`사람을 좋아하는 사교성, 친화력 최고 ${pet.name}`}
+              </S.AdoptText>
+              <S.AdoptBirthAddress>{`${pet.age} ${pet.location}`}</S.AdoptBirthAddress>
+              <S.AdoptLikeBlock>
+                <S.AdoptLike>
+                  <Image
+                    src="/images/pets/like_icon.svg"
+                    alt="like_icon"
+                    width={20}
+                    height={20}
+                  />
+                </S.AdoptLike>
+                {pet.likeCount}
+              </S.AdoptLikeBlock>
+              <S.AdoptViewBlock>
+                <S.AdoptView>
+                  <Image
+                    src="/images/pets/comment_icon.svg"
+                    alt="comment_icon"
+                    width={16}
+                    height={16}
+                  />
+                </S.AdoptView>
+                {pet.commentCount}
+              </S.AdoptViewBlock>
+            </S.AdoptInfoBlock>
+          </S.AdoptPet>
+        ))}
       </S.WrapperContents>
     </>
   );
